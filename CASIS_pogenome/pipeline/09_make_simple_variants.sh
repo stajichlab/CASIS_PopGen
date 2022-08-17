f08e7e1f983e8532583dd12fc4703f515952b221
Jason Stajich <jasonstajich.phd@gmail.com>
2022-08-16 22:32:12 -0700

    write tsv not tab

--- CASIS_pogenome/pipeline/09_make_simple_variants.sh
+++ CASIS_pogenome/pipeline/09_make_simple_variants.sh
@@ -3,6 +3,6 @@
 
 module load bcftools
 module load vcftools
-bcftools view -e 'AF=1' vcf/*.All.SNP.combined_selected.vcf.gz | vcf-to-tab | sort -k1,1 | uniq > vcf/SNP_variants.tab
-bcftools view -e 'AF=1' vcf/*.All.INDEL.combined_selected.vcf.gz | vcf-to-tab | sort -k1,1 | uniq > vcf/INDEL_variants.tab
-cat vcf/SNP_variants.tab vcf/INDEL_variants.tab | sort -k1,1 | uniq > vcf/all_variants.tab
+bcftools view -e 'AF=1' vcf/*.All.SNP.combined_selected.vcf.gz | vcf-to-tab | sort -k1,1 | uniq > vcf/SNP_variants.tsv
+bcftools view -e 'AF=1' vcf/*.All.INDEL.combined_selected.vcf.gz | vcf-to-tab | sort -k1,1 | uniq > vcf/INDEL_variants.tsv
+cat vcf/SNP_variants.tsv vcf/INDEL_variants.tsv | sort -k1,1 | uniq > vcf/all_variants.tsv
